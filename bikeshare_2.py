2b5a091da461af92f2327a39b80e30482041b05c
ubuntu6ty9 <samted.uche@gmail.com>
2023-11-12 00:48:30 +0100

    added type hinting for functions

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -73,7 +73,7 @@ def get_filters():
     return city, month, day
 
 
-def load_data(city, month, day):
+def load_data(city: str, month: str, day: str):
     """
     Loads data for the specified city and filters by month and day if applicable.
 
@@ -108,7 +108,7 @@ def load_data(city, month, day):
     return df
 
 
-def time_stats(df):
+def time_stats(df: pd.DataFrame):
     """Displays statistics on the most frequent times of travel."""
 
     print("\nCalculating The Most Frequent Times of Travel...\n")
@@ -148,7 +148,7 @@ def time_stats(df):
     print("-" * 40)
 
 
-def station_stats(df):
+def station_stats(df: pd.DataFrame):
     """Displays statistics on the most popular stations and trip."""
 
     print("\nCalculating The Most Popular Stations and Trip...\n")
@@ -186,7 +186,7 @@ def station_stats(df):
     print("-" * 40)
 
 
-def trip_duration_stats(df):
+def trip_duration_stats(df: pd.DataFrame):
     """Displays statistics on the total and average trip duration."""
 
     print("\nCalculating Trip Duration...\n")
@@ -212,7 +212,7 @@ def trip_duration_stats(df):
     print("-" * 40)
 
 
-def user_stats(df):
+def user_stats(df: pd.DataFrame):
     """Displays statistics on bikeshare users."""
 
     print("\nCalculating User Stats...\n")
